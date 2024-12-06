999cb685fa03cb23826640422c18423478010ae5
David S. <neojohn2312@gmail.com>
2024-12-05 18:11:48 -0600

    So far so good.

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ npm i
 npx eslint --init
 npm i jest
 npx jest
+npm i -D @types/jest
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,34 @@
+function foo () {
+    return "the foo"
+}
 // Test away!
+describe("our first tests", () => {
+    test("sanity", () =>{
+        expect(5).toBe(4+1)
+        expect(5).toBe(2.5+2.5)
+        expect(5).toBeDefined()
+        expect(5).toBeGreaterThan(4)
+    })
+    test("objects", () => {
+        const o = { a: 1 }
+        const oo = { a: 1 }
+        const ooo = oo
+        expect(o).toBe(o)
+        expect(oo).toBe(ooo)
+    })
+    test("objects again", () => {
+        expect({ a: 1 }).toEqual({ a: 1 })
+    })
+})
+//Unit Testing: Test the smallest pieces of our program,
+//which normally functions and classes.
+// function foo () {
+//     return "the foo"
+// }
+describe("foo function", () => {
+    test("foo returns the foo", () => {
+        expect(foo()).toBe("the foo")
+        expect(foo()).toHaveLength(7)
+    })
+})
+//Car Test:
